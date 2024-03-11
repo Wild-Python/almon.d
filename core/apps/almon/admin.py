0086a6d5df01e45c38cf2d2fd7f19c0e203628b2
elly-hacen <elly.hacen@gmail.com>
2024-03-11 22:03:25 +0500

    Add Category model on admin panel

--- core/apps/almon/admin.py
+++ core/apps/almon/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from .models import Almon
+from .models import Almon, Category
 
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug',)
+    prepopulated_fields = {"slug": ("name",)}
 
 
 @admin.register(Almon)
 class AlmonAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'username',)
+    list_display = ('id', 'user', 'username', 'category',)
